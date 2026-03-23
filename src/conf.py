3c72ebe6207f996c5c04b883533baa0a2994cce3
Aurorp1g <Ze-662607015@outlook.com>
2026-03-23 22:01:43 +0800

    添加：Markdown支持mathjax

--- src/conf.py
+++ src/conf.py
@@ -37,6 +37,9 @@ extensions = ['sphinx.ext.intersphinx',
     'sphinx.ext.githubpages',
     'myst_parser']
 
+myst_enable_extensions = ['amsmath', 'colon_fence', 'dollarmath']
+myst_heading_anchors = 3
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
